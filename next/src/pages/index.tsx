fdfdb8ddb23928f86d03181a62242bb616bdb75a
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-01 22:15:23 +0900

    記事一覧ページの実装

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,19 +1,66 @@
+import { Box, Grid, Container, Pagination } from '@mui/material'
+import camelcaseKeys from 'camelcase-keys'
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import ArticleCard from '@/components/ArticleCard'
+import Loading from '@/components/Loading'
+import Error from '@/components/Error'
+import { styles } from '@/styles'
 import { fetcher } from '@/utils'
 
-const Index: NextPage = () => {
-  const url = 'http://localhost:3000/api/v1/health_check'
-  const { data, error } = useSWR(url, fetcher)
+type ArticleProps = {
+  id: number
+  title: string
+  createdAt: string
+  fromToday: string
+  user: {
+    name: string
+  }
+}
 
-  if (error) return <div>An error has occurred.</div>
-  if (!data) return <div>Loading...</div>
+const Index: NextPage = () => {
+  const router = useRouter()
+  const page = 'page' in router.query ? Number(router.query.page) : 1
+  const url = process.env.NEXT_PUBLIC_API_BASE_URL + '/articles/?page=' + page
+
+  const { data, error } = useSWR(url, fetcher)
+  if (error) return <Error />
+  if (!data) return <Loading />
+
+  const articles = camelcaseKeys(data.articles)
+  const meta = camelcaseKeys(data.meta)
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    router.push('/?page=' + value)
+  }
 
   return (
-    <>
-      <div>Rails疎通確認</div>
-      <div>レスポンスメッセージ: {data.message}</div>
-    </>
+    <Box css={styles.pageMinHeight} sx={{ backgroundColor: '#e6f2ff' }}>
+      <Container maxWidth="md" sx={{ pt: 6 }}>
+        <Grid container spacing={4}>
+          {articles.map((article: ArticleProps, i: number) => (
+            <Grid key={i} item xs={12} md={6}>
+              <Link href={'/articles/' + article.id}>
+                <ArticleCard
+                  title={article.title}
+                  fromToday={article.fromToday}
+                  userName={article.user.name}
+                />
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+        <Box sx={{ display: 'flex', justifyContent: 'center', py: 6 }}>
+          <Pagination
+            count={meta.totalPages}
+            page={meta.currentPage}
+            onChange={handleChange}
+          />
+        </Box>
+      </Container>
+    </Box>
   )
 }
 
