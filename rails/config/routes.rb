3f27131515a58ebcb0559ec81261d76be5b33e7c
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-01 15:28:01 +0900

    devise関連の設定

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
+      namespace :current do
+        resource :user, only: [:show]
+      end
     end
   end
 end
