a39c15319578713f43f1c7b8c05afe568c3a5968
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-01-01 21:17:01 +0900

    Articleモデルの実装。記事一覧詳細取得。マイ記事のCRUD実装。

---
+++ rails/app/controllers/api/v1/current/articles_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::Current::ArticlesController < Api::V1::BaseController
+  before_action :authenticate_user!
+
+  def index
+    articles = current_user.articles.not_unsaved.order(created_at: :desc)
+    render json: articles
+  end
+
+  def show
+    article = current_user.articles.find(params[:id])
+    render json: article
+  end
+ 
+  def create
+    unsaved_article = current_user.articles.unsaved.first || current_user.articles.create!(status: :unsaved)
+    render json: unsaved_article
+  end
+
+  def update
+    article = current_user.articles.find(params[:id])
+    article.update!(article_params)
+    render json: article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :status)
+  end
+end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
       get "health_check", to: "health_check#index"
       mount_devise_token_auth_for "User", at: "auth"
       namespace :current do
-        resource :user, only: [:show]
+        resource :user, only: [:show, :update]
+        resources :articles, only: [:index, :show, :create, :update]
       end
+      resources :articles, only: [:index, :show]
     end
   end
 end
